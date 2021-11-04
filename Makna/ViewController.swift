b4a3fd310cde965a86fa5dd9d96b021feb82744e
waldifebrianda <59052031+waldifebrianda@users.noreply.github.com>
2021-11-04 08:02:50 +0700

    Initial Commit

---
+++ Makna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Makna
+//
+//  Created by Waldi Febrianda on 01/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
